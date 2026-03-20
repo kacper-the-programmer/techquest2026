1e18db8e2d4a9b7b681c2ce7e7e7c53896742ba3
KacperOS <kacper.brzuzan@outlook.com>
2026-03-20 11:07:35 +0100

    aa

--- car.hpp
+++ car.hpp
@@ -5,17 +5,21 @@ class car
 private:
     int width = 50;
     int height = 100;
-    int x = 0;
-    int y = 0;
-
+    float x = 0;
+    float y = 0;
+    int rotation;
     float speed = 0;
+
+    Texture2D texture;
 
 public:
     car()
     {
+        texture = LoadTexture("assets/car.png");
     }
     ~car()
     {
+        UnloadTexture(texture);
     }
     void input()
     {
@@ -29,16 +33,19 @@ public:
         }
         if (IsKeyDown(KEY_W))
         {
-            this->y++;
+            this->y--;
         }
         if (IsKeyDown(KEY_S))
         {
-            this->y--;
+            this->y++;
         }
     }
 
     void draw()
     {
-        DrawRectangle(this->x, this->y, this->width, this->height, RED);
+        // DrawRectangle(this->x, this->y, this->width, this->height, RED);
+        // DrawTexture(texture, x, y, WHITE);
+        Vector2 position = {this->x, this->y};
+        DrawTextureEx(texture, position, 0, 1, WHITE);
     }
 };
